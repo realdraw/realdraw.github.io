832cc6517ba9784a6e56f051ee00e191340849de
tech <tech@realdraw.ai>
2025-11-04 10:42:44 +0900

    Updates

--- _next/static/chunks/app/tags/[tag]/page/[page]/page-00e9b28ea2030a32.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[929],{},_=>{var e=e=>_(_.s=e);_.O(0,[874,447,441,684,358],()=>e(5447)),_N_E=_.O()}]);
---
+++ _next/static/d9lFMvbtj7L_YnIOot4fN/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[...slug]","\u002F[locale]\u002Fblog\u002Fpage\u002F[page]","\u002F[locale]\u002Ftags","\u002F[locale]\u002Ftags\u002F[tag]","\u002F[locale]\u002Ftags\u002F[tag]\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
